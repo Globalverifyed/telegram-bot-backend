7ed0a287939b2eb3d35276f50fb2d069d999bb7c
Globalverifyed <globalverifyed@gmail.com>
2026-04-30 04:45:31 +0600

    Update 9proxy_ip.js

--- handlers/9proxy_ip.js
+++ handlers/9proxy_ip.js
@@ -8,7 +8,7 @@ const proxyIPPackages = {
   ip25: { label: "25 IP", price: "$0.85" },
   ip50: { label: "50 IP", price: "$1.69" },
   ip100: { label: "100 IP", price: "$3.35" },
-  ip200: { label: "200 IP", price: "$7.49" },
+  ip200: { label: "200 IP", price: "$6.49" },
   ip300: { label: "300 IP", price: "$9.59" },
   ip500: { label: "500 IP", price: "$14.99" },
   ip1000: { label: "1000 IP", price: "$27.99" }
