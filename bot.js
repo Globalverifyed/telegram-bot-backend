fdef7ca003a0ccd6834061f95f386fa763aeedc9
Globalverifyed <globalverifyed@gmail.com>
2026-05-03 00:48:52 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -26,6 +26,7 @@ const { handleProxySeller } = require("./handlers/proxy_seller");
 const { handleProxyLight } = require("./handlers/proxy_light");
 const { handleNovProxy } = require("./handlers/nov_proxy");
 const { handleIpRocketProxy } = require("./handlers/iprocket_proxy");
+const { handleNodemaven } = require("./handlers/nodemaven");
 const { handleProductOptions } = require("./handlers/product_options");
 
 const { handleAdmin, handleAdminButtons } = require("./handlers/admin");
@@ -91,6 +92,7 @@ bot.on("callback_query", async (query) => {
   if (await handleProxyLight(bot, query)) return;
   if (await handleNovProxy(bot, query)) return;
   if (await handleIpRocketProxy(bot, query)) return;
+  if (await handleNodemaven(bot, query)) return;
 
   // PRODUCT OPTIONS
   if (await handleProductOptions(bot, query)) return;
