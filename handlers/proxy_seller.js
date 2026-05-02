8c46ca7d4aa01a052b6aa21451492e9fdfcce710
Globalverifyed <globalverifyed@gmail.com>
2026-05-03 00:38:26 +0600

    Create proxy_seller.js

---
+++ handlers/proxy_seller.js
@@ -0,0 +1,81 @@
+const { sendOrEdit } = require("./utils");
+
+const proxySellerDiscountPackages = [
+  { name: "1 GB", price: "$1.5 USDT", callback: "proxy_seller_discount_1gb" },
+  { name: "2 GB", price: "$3 USDT", callback: "proxy_seller_discount_2gb" },
+  { name: "3 GB", price: "$4.5 USDT", callback: "proxy_seller_discount_3gb" },
+  { name: "4 GB", price: "$6 USDT", callback: "proxy_seller_discount_4gb" },
+  { name: "5 GB", price: "$7.5 USDT", callback: "proxy_seller_discount_5gb" }
+];
+
+const proxySellerRegularPackages = [
+  { name: "1 GB", price: "$1.10", callback: "proxy_seller_regular_1gb" },
+  { name: "2 GB", price: "$2.20", callback: "proxy_seller_regular_2gb" },
+  { name: "3 GB", price: "$3.30", callback: "proxy_seller_regular_3gb" },
+  { name: "4 GB", price: "$4.40", callback: "proxy_seller_regular_4gb" },
+  { name: "5 GB", price: "$5.50", callback: "proxy_seller_regular_5gb" },
+  { name: "10 GB", price: "$10.50", callback: "proxy_seller_regular_10gb" },
+  { name: "15 GB", price: "$16.50", callback: "proxy_seller_regular_15gb" },
+  { name: "20 GB", price: "$21.00", callback: "proxy_seller_regular_20gb" }
+];
+
+function makePackageButtons(packages) {
+  const buttons = [];
+
+  for (let i = 0; i < packages.length; i += 2) {
+    buttons.push(
+      packages.slice(i, i + 2).map((pkg) => ({
+        text: `🟢 ${pkg.name} - ${pkg.price}`,
+        callback_data: pkg.callback
+      }))
+    );
+  }
+
+  buttons.push([{ text: "⬅️ Back", callback_data: "proxy_seller" }]);
+
+  return buttons;
+}
+
+async function handleProxySeller(bot, query) {
+  const data = query.data;
+
+  if (data === "proxy_seller") {
+    await sendOrEdit(bot, query, "🌐 Proxy Seller\n\nPlease select price type:", [
+      [
+        { text: "🔥 Discount Price", callback_data: "proxy_seller_discount_menu" },
+        { text: "💎 Regular Price", callback_data: "proxy_seller_regular_menu" }
+      ],
+      [
+        { text: "⬅️ Back", callback_data: "ip_proxy" }
+      ]
+    ]);
+
+    return true;
+  }
+
+  if (data === "proxy_seller_discount_menu") {
+    await sendOrEdit(
+      bot,
+      query,
+      "🔥 Proxy Seller Discount Price List\n\nSelect your package:",
+      makePackageButtons(proxySellerDiscountPackages)
+    );
+
+    return true;
+  }
+
+  if (data === "proxy_seller_regular_menu") {
+    await sendOrEdit(
+      bot,
+      query,
+      "💎 Proxy Seller Regular Price List\n\nSelect your package:",
+      makePackageButtons(proxySellerRegularPackages)
+    );
+
+    return true;
+  }
+
+  return false;
+}
+
+module.exports = { handleProxySeller };
