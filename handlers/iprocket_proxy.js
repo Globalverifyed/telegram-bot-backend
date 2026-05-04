89c010bed2a4401c1208e9ea02f6d16c96019e7f
Globalverifyed <globalverifyed@gmail.com>
2026-05-03 20:09:01 -0400

    Update iprocket_proxy.js

--- handlers/iprocket_proxy.js
+++ handlers/iprocket_proxy.js
@@ -1,22 +1,22 @@
 const { sendOrEdit } = require("./utils");
 
 const iprocketDiscountPackages = [
-  { name: "1 GB", price: "$1.5 USDT", callback: "iprocket_discount_1gb" },
-  { name: "2 GB", price: "$3 USDT", callback: "iprocket_discount_2gb" },
-  { name: "3 GB", price: "$4.5 USDT", callback: "iprocket_discount_3gb" },
-  { name: "4 GB", price: "$6 USDT", callback: "iprocket_discount_4gb" },
-  { name: "5 GB", price: "$7.5 USDT", callback: "iprocket_discount_5gb" }
+  { name: "2 GB", price: "$1.60 USDT", callback: "iprocket_discount_2gb" },
+  { name: "4 GB", price: "$3.20 USDT", callback: "iprocket_discount_4gb" },
+  { name: "6 GB", price: "$4.80 USDT", callback: "iprocket_discount_6gb" },
+  { name: "8 GB", price: "$6.40 USDT", callback: "iprocket_discount_8gb" },
+  { name: "10 GB", price: "$8.00 USDT", callback: "iprocket_discount_10gb" }
 ];
 
 const iprocketRegularPackages = [
-  { name: "1 GB", price: "$1.10", callback: "iprocket_regular_1gb" },
-  { name: "2 GB", price: "$2.20", callback: "iprocket_regular_2gb" },
-  { name: "3 GB", price: "$3.30", callback: "iprocket_regular_3gb" },
-  { name: "4 GB", price: "$4.40", callback: "iprocket_regular_4gb" },
-  { name: "5 GB", price: "$5.50", callback: "iprocket_regular_5gb" },
-  { name: "10 GB", price: "$10.50", callback: "iprocket_regular_10gb" },
-  { name: "15 GB", price: "$16.50", callback: "iprocket_regular_15gb" },
-  { name: "20 GB", price: "$21.00", callback: "iprocket_regular_20gb" }
+  { name: "2 GB", price: "$2.00", callback: "iprocket_regular_2gb" },
+  { name: "4 GB", price: "$4.00", callback: "iprocket_regular_4gb" },
+  { name: "6 GB", price: "$6.00", callback: "iprocket_regular_6gb" },
+  { name: "8 GB", price: "$8.00", callback: "iprocket_regular_8gb" },
+  { name: "10 GB", price: "$10.00", callback: "iprocket_regular_10gb" },
+  { name: "16 GB", price: "$16.00", callback: "iprocket_regular_16gb" },
+  { name: "20 GB", price: "$20.00", callback: "iprocket_regular_20gb" },
+  { name: "30 GB", price: "$30.00", callback: "iprocket_regular_30gb" }
 ];
 
 function makePackageButtons(packages) {
