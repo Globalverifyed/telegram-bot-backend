475e939aef9ea51d72442341dcd1265c082f2bc3
Globalverifyed <globalverifyed@gmail.com>
2026-05-05 05:39:12 +0600

    Update cliproxy.js

--- handlers/cliproxy.js
+++ handlers/cliproxy.js
@@ -1,22 +1,22 @@
 const { sendOrEdit } = require("./utils");
 
 const cliProxyDiscountPackages = [
-  { name: "1 GB", price: "$1.5 USDT", callback: "cliproxy_discount_1gb" },
-  { name: "2 GB", price: "$3 USDT", callback: "cliproxy_discount_2gb" },
-  { name: "3 GB", price: "$4.5 USDT", callback: "cliproxy_discount_3gb" },
-  { name: "4 GB", price: "$6 USDT", callback: "cliproxy_discount_4gb" },
-  { name: "5 GB", price: "$7.5 USDT", callback: "cliproxy_discount_5gb" }
+  { name: "25 IP", price: "$2.00 USDT", callback: "cliproxy_discount_25ip" },
+  { name: "50 IP", price: "$4.00 USDT", callback: "cliproxy_discount_50ip" },
+  { name: "100 IP", price: "$8.00 USDT", callback: "cliproxy_discount_100ip" },
+  { name: "200 IP", price: "$16.00 USDT", callback: "cliproxy_discount_200ip" },
+  { name: "400 IP", price: "$32.00 USDT", callback: "cliproxy_discount_400ip" }
 ];
 
 const cliProxyRegularPackages = [
-  { name: "1 GB", price: "$1.10", callback: "cliproxy_regular_1gb" },
-  { name: "2 GB", price: "$2.20", callback: "cliproxy_regular_2gb" },
-  { name: "3 GB", price: "$3.30", callback: "cliproxy_regular_3gb" },
-  { name: "4 GB", price: "$4.40", callback: "cliproxy_regular_4gb" },
-  { name: "5 GB", price: "$5.50", callback: "cliproxy_regular_5gb" },
-  { name: "10 GB", price: "$10.50", callback: "cliproxy_regular_10gb" },
-  { name: "15 GB", price: "$16.50", callback: "cliproxy_regular_15gb" },
-  { name: "20 GB", price: "$21.00", callback: "cliproxy_regular_20gb" }
+  { name: "25 IP", price: "$3.00", callback: "cliproxy_regular_25ip" },
+  { name: "50 IP", price: "$6.00", callback: "cliproxy_regular_50ip" },
+  { name: "100 IP", price: "$12.00", callback: "cliproxy_regular_100ip" },
+  { name: "200 IP", price: "$22.00", callback: "cliproxy_regular_200ip" },
+  { name: "400 IP", price: "$42.00", callback: "cliproxy_regular_400ip" },
+  { name: "800 IP", price: "$78.00", callback: "cliproxy_regular_800ip" },
+  { name: "1000 IP", price: "$88.00", callback: "cliproxy_regular_1000ip" },
+  { name: "1200 IP", price: "$99.00", callback: "cliproxy_regular_1200ip" }
 ];
 
 function makePackageButtons(packages) {
