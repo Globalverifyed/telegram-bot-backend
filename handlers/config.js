cdd5b18b0580a2390145cedf2744fb17750bce06
Globalverifyed <globalverifyed@gmail.com>
2026-05-07 13:59:27 -0400

    Update config.js

--- handlers/config.js
+++ handlers/config.js
@@ -1,6 +1,6 @@
 // config.js
 module.exports = {
-  CHANNEL_ID: -1003704005774, // আপনার channel ID (negative for supergroup/channel)
+  CHANNEL_ID: -1003704005774, // তোমার channel ID
   CHANNEL_LINK: process.env.CHANNEL_URL,
   SUPPORT_URL: process.env.SUPPORT_URL,
   ADMIN_CHAT_ID: process.env.ADMIN_CHAT_ID
