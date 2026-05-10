bdf5142b0286b8f51809e96d702c0eb86482665c
Globalverifyed <globalverifyed@gmail.com>
2026-05-10 06:08:53 +0600

    Update product_options.js

--- handlers/product_options.js
+++ handlers/product_options.js
@@ -19,25 +19,25 @@ const proxyProducts = {
   // 🌍 9PROXY IP
   // Account type লাগবে: OLD / New / Redeem
   // =========================
-  proxy_ip_discount_25: { productKey: "proxy_ip", itemKey: "ip_d_25", name: "9proxy IP", package: "25 IP Discount", price: "$0.85", back: "proxy_ip_discount_menu" },
-  proxy_ip_discount_50: { productKey: "proxy_ip", itemKey: "ip_d_50", name: "9proxy IP", package: "50 IP Discount", price: "$1.69", back: "proxy_ip_discount_menu" },
-  proxy_ip_discount_100: { productKey: "proxy_ip", itemKey: "ip_d_100", name: "9proxy IP", package: "100 IP Discount", price: "$3.35", back: "proxy_ip_discount_menu" },
-  proxy_ip_discount_200: { productKey: "proxy_ip", itemKey: "ip_d_200", name: "9proxy IP", package: "200 IP Discount", price: "$6.49", back: "proxy_ip_discount_menu" },
-  proxy_ip_discount_300: { productKey: "proxy_ip", itemKey: "ip_d_300", name: "9proxy IP", package: "300 IP Discount", price: "$9.59", back: "proxy_ip_discount_menu" },
-  proxy_ip_discount_500: { productKey: "proxy_ip", itemKey: "ip_d_500", name: "9proxy IP", package: "500 IP Discount", price: "$14.99", back: "proxy_ip_discount_menu" },
-  proxy_ip_discount_1000: { productKey: "proxy_ip", itemKey: "ip_d_1000", name: "9proxy IP", package: "1000 IP Discount", price: "$27.99", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_25: { productKey: "proxy_ip", itemKey: "ip_d_25", name: "9proxy IP", package: "25 IP Discount", price: "$0.79", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_50: { productKey: "proxy_ip", itemKey: "ip_d_50", name: "9proxy IP", package: "50 IP Discount", price: "$1.49", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_100: { productKey: "proxy_ip", itemKey: "ip_d_100", name: "9proxy IP", package: "100 IP Discount", price: "$2.49", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_200: { productKey: "proxy_ip", itemKey: "ip_d_200", name: "9proxy IP", package: "200 IP Discount", price: "$4.79", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_300: { productKey: "proxy_ip", itemKey: "ip_d_300", name: "9proxy IP", package: "300 IP Discount", price: "$7.10", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_500: { productKey: "proxy_ip", itemKey: "ip_d_500", name: "9proxy IP", package: "500 IP Discount", price: "$11.90", back: "proxy_ip_discount_menu" },
+  proxy_ip_discount_1000: { productKey: "proxy_ip", itemKey: "ip_d_1000", name: "9proxy IP", package: "1000 IP Discount", price: "$23.69", back: "proxy_ip_discount_menu" },
 
   // =========================
   // 📦 9PROXY GB
   // Account type লাগবে: OLD / New / Redeem
   // =========================
-  proxy_gb_discount_1: { productKey: "proxy_gb", itemKey: "gb_d_1", name: "9proxy GB", package: "1GB Discount", price: "$0.80", back: "proxy_gb_discount_menu" },
-  proxy_gb_discount_2: { productKey: "proxy_gb", itemKey: "gb_d_2", name: "9proxy GB", package: "2GB Discount", price: "$1.60", back: "proxy_gb_discount_menu" },
-  proxy_gb_discount_3: { productKey: "proxy_gb", itemKey: "gb_d_3", name: "9proxy GB", package: "3GB Discount", price: "$2.40", back: "proxy_gb_discount_menu" },
-  proxy_gb_discount_5: { productKey: "proxy_gb", itemKey: "gb_d_5", name: "9proxy GB", package: "5GB Discount", price: "$3.99", back: "proxy_gb_discount_menu" },
-  proxy_gb_discount_10: { productKey: "proxy_gb", itemKey: "gb_d_10", name: "9proxy GB", package: "10GB Discount", price: "$7.99", back: "proxy_gb_discount_menu" },
-  proxy_gb_discount_15: { productKey: "proxy_gb", itemKey: "gb_d_15", name: "9proxy GB", package: "15GB Discount", price: "$11.99", back: "proxy_gb_discount_menu" },
-  proxy_gb_discount_20: { productKey: "proxy_gb", itemKey: "gb_d_20", name: "9proxy GB", package: "20GB Discount", price: "$15.99", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_1: { productKey: "proxy_gb", itemKey: "gb_d_1", name: "9proxy GB", package: "1GB Discount", price: "$1.00", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_2: { productKey: "proxy_gb", itemKey: "gb_d_2", name: "9proxy GB", package: "2GB Discount", price: "$2.00", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_3: { productKey: "proxy_gb", itemKey: "gb_d_3", name: "9proxy GB", package: "3GB Discount", price: "$3.00", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_5: { productKey: "proxy_gb", itemKey: "gb_d_5", name: "9proxy GB", package: "5GB Discount", price: "$5.00", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_10: { productKey: "proxy_gb", itemKey: "gb_d_10", name: "9proxy GB", package: "10GB Discount", price: "$10.00", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_15: { productKey: "proxy_gb", itemKey: "gb_d_15", name: "9proxy GB", package: "15GB Discount", price: "$15.00", back: "proxy_gb_discount_menu" },
+  proxy_gb_discount_20: { productKey: "proxy_gb", itemKey: "gb_d_20", name: "9proxy GB", package: "20GB Discount", price: "$20.00", back: "proxy_gb_discount_menu" },
   // =========================
   // ABC PROXY
   // =========================
@@ -60,56 +60,56 @@ abc_discount_10gb: { productKey: "abc_proxy", itemKey: "abc_d_10", name: "ABC Pr
   // =========================
   // PROXY SELLER
   // =========================
-  proxy_seller_discount_1gb: { productKey: "proxy_seller", itemKey: "ps_d_1", name: "Proxy Seller", package: "1GB Discount", price: "$1.5", back: "proxy_seller_discount_menu" },
-  proxy_seller_discount_2gb: { productKey: "proxy_seller", itemKey: "ps_d_2", name: "Proxy Seller", package: "2GB Discount", price: "$3", back: "proxy_seller_discount_menu" },
-  proxy_seller_discount_3gb: { productKey: "proxy_seller", itemKey: "ps_d_3", name: "Proxy Seller", package: "3GB Discount", price: "$4.5", back: "proxy_seller_discount_menu" },
-  proxy_seller_discount_4gb: { productKey: "proxy_seller", itemKey: "ps_d_4", name: "Proxy Seller", package: "4GB Discount", price: "$6", back: "proxy_seller_discount_menu" },
-  proxy_seller_discount_5gb: { productKey: "proxy_seller", itemKey: "ps_d_5", name: "Proxy Seller", package: "5GB Discount", price: "$7.5", back: "proxy_seller_discount_menu" },
+  proxy_seller_discount_1gb: { productKey: "proxy_seller", itemKey: "ps_d_1", name: "Proxy Seller", package: "1GB Discount", price: "$2.00", back: "proxy_seller_discount_menu" },
+  proxy_seller_discount_2gb: { productKey: "proxy_seller", itemKey: "ps_d_2", name: "Proxy Seller", package: "2GB Discount", price: "$4.00", back: "proxy_seller_discount_menu" },
+  proxy_seller_discount_3gb: { productKey: "proxy_seller", itemKey: "ps_d_3", name: "Proxy Seller", package: "3GB Discount", price: "$5.90", back: "proxy_seller_discount_menu" },
+  proxy_seller_discount_4gb: { productKey: "proxy_seller", itemKey: "ps_d_4", name: "Proxy Seller", package: "4GB Discount", price: "$7.85", back: "proxy_seller_discount_menu" },
+  proxy_seller_discount_5gb: { productKey: "proxy_seller", itemKey: "ps_d_5", name: "Proxy Seller", package: "5GB Discount", price: "$9.80", back: "proxy_seller_discount_menu" },
 
-  proxy_seller_regular_1gb: { productKey: "proxy_seller", itemKey: "ps_r_1", name: "Proxy Seller", package: "1GB Regular", price: "$1.10", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_2gb: { productKey: "proxy_seller", itemKey: "ps_r_2", name: "Proxy Seller", package: "2GB Regular", price: "$2.20", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_3gb: { productKey: "proxy_seller", itemKey: "ps_r_3", name: "Proxy Seller", package: "3GB Regular", price: "$3.30", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_4gb: { productKey: "proxy_seller", itemKey: "ps_r_4", name: "Proxy Seller", package: "4GB Regular", price: "$4.40", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_5gb: { productKey: "proxy_seller", itemKey: "ps_r_5", name: "Proxy Seller", package: "5GB Regular", price: "$5.50", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_10gb: { productKey: "proxy_seller", itemKey: "ps_r_10", name: "Proxy Seller", package: "10GB Regular", price: "$10.50", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_15gb: { productKey: "proxy_seller", itemKey: "ps_r_15", name: "Proxy Seller", package: "15GB Regular", price: "$16.50", back: "proxy_seller_regular_menu" },
-  proxy_seller_regular_20gb: { productKey: "proxy_seller", itemKey: "ps_r_20", name: "Proxy Seller", package: "20GB Regular", price: "$21.00", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_1gb: { productKey: "proxy_seller", itemKey: "ps_r_1", name: "Proxy Seller", package: "1GB Regular", price: "$2.50", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_2gb: { productKey: "proxy_seller", itemKey: "ps_r_2", name: "Proxy Seller", package: "2GB Regular", price: "$5.00", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_3gb: { productKey: "proxy_seller", itemKey: "ps_r_3", name: "Proxy Seller", package: "3GB Regular", price: "$7.50", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_4gb: { productKey: "proxy_seller", itemKey: "ps_r_4", name: "Proxy Seller", package: "4GB Regular", price: "$9.50", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_5gb: { productKey: "proxy_seller", itemKey: "ps_r_5", name: "Proxy Seller", package: "5GB Regular", price: "$11.50", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_10gb: { productKey: "proxy_seller", itemKey: "ps_r_10", name: "Proxy Seller", package: "10GB Regular", price: "$22.50", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_15gb: { productKey: "proxy_seller", itemKey: "ps_r_15", name: "Proxy Seller", package: "15GB Regular", price: "$33.00", back: "proxy_seller_regular_menu" },
+  proxy_seller_regular_20gb: { productKey: "proxy_seller", itemKey: "ps_r_20", name: "Proxy Seller", package: "20GB Regular", price: "$42.00", back: "proxy_seller_regular_menu" },
 
   // =========================
   // PROXY LIGHT
   // =========================
-  proxy_light_discount_1gb: { productKey: "proxy_light", itemKey: "pl_d_1", name: "Proxy Light", package: "1GB Discount", price: "$1.5", back: "proxy_light_discount_menu" },
-  proxy_light_discount_2gb: { productKey: "proxy_light", itemKey: "pl_d_2", name: "Proxy Light", package: "2GB Discount", price: "$3", back: "proxy_light_discount_menu" },
-  proxy_light_discount_3gb: { productKey: "proxy_light", itemKey: "pl_d_3", name: "Proxy Light", package: "3GB Discount", price: "$4.5", back: "proxy_light_discount_menu" },
-  proxy_light_discount_4gb: { productKey: "proxy_light", itemKey: "pl_d_4", name: "Proxy Light", package: "4GB Discount", price: "$6", back: "proxy_light_discount_menu" },
-  proxy_light_discount_5gb: { productKey: "proxy_light", itemKey: "pl_d_5", name: "Proxy Light", package: "5GB Discount", price: "$7.5", back: "proxy_light_discount_menu" },
+  proxy_light_discount_1gb: { productKey: "proxy_light", itemKey: "pl_d_1", name: "Proxy Light", package: "1GB Discount", price: "$3.50", back: "proxy_light_discount_menu" },
+  proxy_light_discount_2gb: { productKey: "proxy_light", itemKey: "pl_d_2", name: "Proxy Light", package: "2GB Discount", price: "$7.00", back: "proxy_light_discount_menu" },
+  proxy_light_discount_3gb: { productKey: "proxy_light", itemKey: "pl_d_3", name: "Proxy Light", package: "3GB Discount", price: "$10.50", back: "proxy_light_discount_menu" },
+  proxy_light_discount_4gb: { productKey: "proxy_light", itemKey: "pl_d_4", name: "Proxy Light", package: "4GB Discount", price: "$14.00", back: "proxy_light_discount_menu" },
+  proxy_light_discount_5gb: { productKey: "proxy_light", itemKey: "pl_d_5", name: "Proxy Light", package: "5GB Discount", price: "$17.50", back: "proxy_light_discount_menu" },
 
-  proxy_light_regular_1gb: { productKey: "proxy_light", itemKey: "pl_r_1", name: "Proxy Light", package: "1GB Regular", price: "$1.10", back: "proxy_light_regular_menu" },
-  proxy_light_regular_2gb: { productKey: "proxy_light", itemKey: "pl_r_2", name: "Proxy Light", package: "2GB Regular", price: "$2.20", back: "proxy_light_regular_menu" },
-  proxy_light_regular_3gb: { productKey: "proxy_light", itemKey: "pl_r_3", name: "Proxy Light", package: "3GB Regular", price: "$3.30", back: "proxy_light_regular_menu" },
-  proxy_light_regular_4gb: { productKey: "proxy_light", itemKey: "pl_r_4", name: "Proxy Light", package: "4GB Regular", price: "$4.40", back: "proxy_light_regular_menu" },
-  proxy_light_regular_5gb: { productKey: "proxy_light", itemKey: "pl_r_5", name: "Proxy Light", package: "5GB Regular", price: "$5.50", back: "proxy_light_regular_menu" },
-  proxy_light_regular_10gb: { productKey: "proxy_light", itemKey: "pl_r_10", name: "Proxy Light", package: "10GB Regular", price: "$10.50", back: "proxy_light_regular_menu" },
-  proxy_light_regular_15gb: { productKey: "proxy_light", itemKey: "pl_r_15", name: "Proxy Light", package: "15GB Regular", price: "$16.50", back: "proxy_light_regular_menu" },
-  proxy_light_regular_20gb: { productKey: "proxy_light", itemKey: "pl_r_20", name: "Proxy Light", package: "20GB Regular", price: "$21.00", back: "proxy_light_regular_menu" },
+  proxy_light_regular_1gb: { productKey: "proxy_light", itemKey: "pl_r_1", name: "Proxy Light", package: "1GB Regular", price: "$4.50", back: "proxy_light_regular_menu" },
+  proxy_light_regular_2gb: { productKey: "proxy_light", itemKey: "pl_r_2", name: "Proxy Light", package: "2GB Regular", price: "$8.50", back: "proxy_light_regular_menu" },
+  proxy_light_regular_3gb: { productKey: "proxy_light", itemKey: "pl_r_3", name: "Proxy Light", package: "3GB Regular", price: "$13.00", back: "proxy_light_regular_menu" },
+  proxy_light_regular_4gb: { productKey: "proxy_light", itemKey: "pl_r_4", name: "Proxy Light", package: "4GB Regular", price: "$16.50", back: "proxy_light_regular_menu" },
+  proxy_light_regular_5gb: { productKey: "proxy_light", itemKey: "pl_r_5", name: "Proxy Light", package: "5GB Regular", price: "$19.50", back: "proxy_light_regular_menu" },
+  proxy_light_regular_10gb: { productKey: "proxy_light", itemKey: "pl_r_10", name: "Proxy Light", package: "10GB Regular", price: "$31.50", back: "proxy_light_regular_menu" },
+  proxy_light_regular_15gb: { productKey: "proxy_light", itemKey: "pl_r_15", name: "Proxy Light", package: "15GB Regular", price: "$46.00", back: "proxy_light_regular_menu" },
+  proxy_light_regular_20gb: { productKey: "proxy_light", itemKey: "pl_r_20", name: "Proxy Light", package: "20GB Regular", price: "$62.00", back: "proxy_light_regular_menu" },
 
   // =========================
   // NOV PROXY
   // =========================
-  nov_proxy_discount_1gb: { productKey: "nov_proxy", itemKey: "nv_d_1", name: "Nov Proxy", package: "1GB Discount", price: "$1.5", back: "nov_proxy_discount_menu" },
-  nov_proxy_discount_2gb: { productKey: "nov_proxy", itemKey: "nv_d_2", name: "Nov Proxy", package: "2GB Discount", price: "$3", back: "nov_proxy_discount_menu" },
-  nov_proxy_discount_3gb: { productKey: "nov_proxy", itemKey: "nv_d_3", name: "Nov Proxy", package: "3GB Discount", price: "$4.5", back: "nov_proxy_discount_menu" },
-  nov_proxy_discount_4gb: { productKey: "nov_proxy", itemKey: "nv_d_4", name: "Nov Proxy", package: "4GB Discount", price: "$6", back: "nov_proxy_discount_menu" },
-  nov_proxy_discount_5gb: { productKey: "nov_proxy", itemKey: "nv_d_5", name: "Nov Proxy", package: "5GB Discount", price: "$7.5", back: "nov_proxy_discount_menu" },
+  nov_proxy_discount_1gb: { productKey: "nov_proxy", itemKey: "nv_d_25", name: "Nov Proxy", package: "25 IP Discount", price: "$2.00", back: "nov_proxy_discount_menu" },
+  nov_proxy_discount_2gb: { productKey: "nov_proxy", itemKey: "nv_d_50", name: "Nov Proxy", package: "50 IP Discount", price: "$4.00", back: "nov_proxy_discount_menu" },
+  nov_proxy_discount_3gb: { productKey: "nov_proxy", itemKey: "nv_d_100", name: "Nov Proxy", package: "100 IP Discount", price: "$8.00", back: "nov_proxy_discount_menu" },
+  nov_proxy_discount_4gb: { productKey: "nov_proxy", itemKey: "nv_d_200", name: "Nov Proxy", package: "200 IP Discount", price: "$16.00", back: "nov_proxy_discount_menu" },
+  nov_proxy_discount_5gb: { productKey: "nov_proxy", itemKey: "nv_d_400", name: "Nov Proxy", package: "400 IP Discount", price: "$32.00", back: "nov_proxy_discount_menu" },
 
-  nov_proxy_regular_1gb: { productKey: "nov_proxy", itemKey: "nv_r_1", name: "Nov Proxy", package: "1GB Regular", price: "$1.10", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_2gb: { productKey: "nov_proxy", itemKey: "nv_r_2", name: "Nov Proxy", package: "2GB Regular", price: "$2.20", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_3gb: { productKey: "nov_proxy", itemKey: "nv_r_3", name: "Nov Proxy", package: "3GB Regular", price: "$3.30", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_4gb: { productKey: "nov_proxy", itemKey: "nv_r_4", name: "Nov Proxy", package: "4GB Regular", price: "$4.40", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_5gb: { productKey: "nov_proxy", itemKey: "nv_r_5", name: "Nov Proxy", package: "5GB Regular", price: "$5.50", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_10gb: { productKey: "nov_proxy", itemKey: "nv_r_10", name: "Nov Proxy", package: "10GB Regular", price: "$10.50", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_15gb: { productKey: "nov_proxy", itemKey: "nv_r_15", name: "Nov Proxy", package: "15GB Regular", price: "$16.50", back: "nov_proxy_regular_menu" },
-  nov_proxy_regular_20gb: { productKey: "nov_proxy", itemKey: "nv_r_20", name: "Nov Proxy", package: "20GB Regular", price: "$21.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_1gb: { productKey: "nov_proxy", itemKey: "nv_r_25", name: "Nov Proxy", package: "25 IP Regular", price: "$3.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_2gb: { productKey: "nov_proxy", itemKey: "nv_r_50", name: "Nov Proxy", package: "50 IP Regular", price: "$6.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_3gb: { productKey: "nov_proxy", itemKey: "nv_r_100", name: "Nov Proxy", package: "100 IP Regular", price: "$12.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_4gb: { productKey: "nov_proxy", itemKey: "nv_r_200", name: "Nov Proxy", package: "200 IP Regular", price: "$22.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_5gb: { productKey: "nov_proxy", itemKey: "nv_r_400", name: "Nov Proxy", package: "400 IP Regular", price: "$42.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_10gb: { productKey: "nov_proxy", itemKey: "nv_r_800", name: "Nov Proxy", package: "800 IP Regular", price: "$78.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_15gb: { productKey: "nov_proxy", itemKey: "nv_r_1000", name: "Nov Proxy", package: "1000 IP Regular", price: "$88.00", back: "nov_proxy_regular_menu" },
+  nov_proxy_regular_20gb: { productKey: "nov_proxy", itemKey: "nv_r_1200", name: "Nov Proxy", package: "1200 IP Regular", price: "$99.00", back: "nov_proxy_regular_menu" },
 
   // =========================
   // IPROCKET PROXY
