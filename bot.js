4d288fb6808d13cfdab61f1ba13e692ed6adc19c
Globalverifyed <globalverifyed@gmail.com>
2026-05-12 00:48:33 +0600

    Update bot.js

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ const { ADMIN_CHAT_ID } = require("./config");
 
 const { forceJoin } = require("./handlers/force_join");
 const { showMainMenu } = require("./handlers/menu");
-const { trackUser } = require("./handlers/sheet_tracker");
+const { trackUser, trackOrder } = require("./handlers/sheet_tracker");
 
 const {
   handleSupport,
